8298b90e0dd96c33a444dd15f5a263774d9fd0e3
orakili <github@orakili.net>
2024-09-06 06:53:51 +0000

    chore: remove unused server script and change port for local setup
    Refs: OPS-10771

--- html/server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# @todo handle additional parameters.
-uvicorn app:app \
-  --host ${SERVER_HOST:-0.0.0.0} \
-  --port ${SERVER_PORT:-80} \
-  --log-config=log_config.yaml \
-  --reload
